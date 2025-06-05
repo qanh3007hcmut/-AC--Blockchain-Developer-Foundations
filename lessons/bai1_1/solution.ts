b03fb0c4823b0417b4af76de8fc3d3561d4a50e3
qanh3007hcmut <qanh3007@gmail.com>
2025-06-05 10:45:35 +0700

    Do bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,13 +1,21 @@
 
 export type Block = {
-  index: number;
-  timestamp: string;
-  transactions: any[];
-  previous_hash: string;
-  current_hash: string;
+	index: number;
+	timestamp: string;
+	transactions: any[];
+	previous_hash: string;
+	current_hash: string;
 };
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+	let hashVal = ""
+	Object.entries(block).forEach(([key, value]) => {
+		hashVal += key != "current_hash" ? 
+					(key == "transactions" ? JSON.stringify(value) : value + "") 
+					: "";
+		// console.log(hashVal);
+	});
+	const hash = crypto.createHash('sha256').update(hashVal).digest('hex')
+	return hash === block.current_hash ? true : false; // Chỉnh lại logic
 }
